856585362374b817999e18bc8b19bda7c5f236ae
Patrick Michiels <it@patrickmichiels.de>
2024-10-12 09:39:54 +0200

    Added Basic AppBar to MealsDailyOverviewScreen

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 const kColors_primary = Color(0xFFBE1C37);
 const kColors_secondary = Colors.grey;
 const kColors_backgroundColorGrey = Color(0xFFF0F0F0);
+const kColors_backNavigationIconColor = Colors.black;
+const kColors_backNavigationTextColor = Colors.black;
+const kColors_defaultTextColor = Colors.black;
 
 /// Login screen colours.
 const kColors_flutterLogin_inputDecorationTheme_color = Color(0xFF757575); // Colors.grey.shade600
@@ -11,6 +14,10 @@ const kColors_welcomeScreen_inputBackgroundColor = Color(0x172196f3);
 const kColors_flutterLoginTheme_pageColorLight = Color(0xFF757575);
 const kColors_flutterLoginTheme_primaryColor = Color(0xFFBE1C37);
 const kColors_flutterLoginTheme_titleTextColor = Color(0xFFDEE3D9);
+
+
+/// MealsDailyOverviewScreen colours.
+const kColors_mealsOverViewScreenDaily_appBar_progressBar_color = kColors_primary;
 
 
 
--- lib/screens/meals_overview_daily.dart
+++ lib/screens/meals_overview_daily.dart
@@ -1,19 +1,23 @@
-// Public packages imports.
+// Public package imports.
 import 'package:provider/provider.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart' hide NavigationMode;
 
 // Own package imports.
 import 'package:engaige_meal_tracker_demo/constants/colors.dart';
-import 'package:engaige_meal_tracker_demo/providers/ui_readiness_provider.dart';
-import 'package:engaige_meal_tracker_demo/widgets/scroll_to_top_fab.dart';
 import 'package:engaige_meal_tracker_demo/models/user.dart';
+import 'package:engaige_meal_tracker_demo/providers/ui_readiness_provider.dart';
+import 'package:engaige_meal_tracker_demo/providers/navigation_provider.dart';
 import 'package:engaige_meal_tracker_demo/providers/data_provider.dart';
+import 'package:engaige_meal_tracker_demo/widgets/scroll_to_top_fab.dart';
+import 'package:engaige_meal_tracker_demo/widgets/language_switcher.dart';
 
 /// A screen that displays the daily overview of meals.
 ///
-/// This screen shows meal details for the day and provides a scrollable interface.
-/// It includes a floating action button that scrolls the view to the top when pressed.
+/// This screen provides a scrollable interface for viewing the details of daily meals.
+/// It includes a floating action button (FAB) that allows users to quickly scroll
+/// to the top of the screen. Additionally, the screen shows localized content and
+/// provides a language switcher in the app bar.
 class MealsDailyOverviewScreen extends StatefulWidget {
   static const String id = 'meals_overview_daily_screen';
 
@@ -24,9 +28,9 @@ class MealsDailyOverviewScreen extends StatefulWidget {
 }
 
 class _MealsDailyOverviewScreenState extends State<MealsDailyOverviewScreen> {
-  /// Future to track the readiness of the notification settings screen.
+  /// Future to track the readiness of the `MealsDailyOverviewScreen`.
   /// The readiness flag is awaited before building the screen content.
-  late Future<void> _notificationSettingsScreenValuesAreReady;
+  late Future<void> _mealsDailyOverviewScreenValuesAreReady;
 
   /// The user object holding logged-in user details.
   late final User user;
@@ -56,11 +60,11 @@ class _MealsDailyOverviewScreenState extends State<MealsDailyOverviewScreen> {
       }
     });
 
-    // Fetch required values for displaying meal overview.
+    // Fetch required values for displaying the meal overview.
     _getData();
 
     // Await the readiness of the Meals Overview Daily Screen before displaying content.
-    _notificationSettingsScreenValuesAreReady =
+    _mealsDailyOverviewScreenValuesAreReady =
         Provider.of<UIReadinessProvider>(context, listen: false)
             .awaitMealsOverviewDailyReadiness();
   }
@@ -68,7 +72,7 @@ class _MealsDailyOverviewScreenState extends State<MealsDailyOverviewScreen> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<void>(
-      future: _notificationSettingsScreenValuesAreReady,
+      future: _mealsDailyOverviewScreenValuesAreReady,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           // Show a loading indicator while waiting for the screen to be ready.
@@ -92,6 +96,7 @@ class _MealsDailyOverviewScreenState extends State<MealsDailyOverviewScreen> {
   Scaffold buildScreenAfterFutureResolved(BuildContext context) {
     return Scaffold(
       backgroundColor: kColors_backgroundColorGrey,
+      appBar: _buildCustomAppBar(context),
       floatingActionButton: ScrollToTopFloatingActionButton(
         scrollController: scrollController,
         showScrollToTopBtn: showScrollToTopBtn,
@@ -104,13 +109,101 @@ class _MealsDailyOverviewScreenState extends State<MealsDailyOverviewScreen> {
     );
   }
 
+  /// Builds a custom `AppBar` for the `MealsDailyOverviewScreen`.
+  PreferredSizeWidget _buildCustomAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: kColors_backgroundColorGrey,
+      elevation: 0.2,
+      leading: _buildAppBarLeadingWidget(context),
+      title: _buildAppBarTitleWidget(),
+      actions: [_buildLanguageSwitcher(context)],
+      bottom: _buildAppBarBottomWidget(),
+    );
+  }
+
+  /// Builds the leading widget for the `AppBar`, which includes a back button
+  /// and a label for back navigation.
+  Row _buildAppBarLeadingWidget(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          icon: const Icon(Icons.arrow_back),
+          color: kColors_backNavigationIconColor,
+          onPressed: () {
+            Provider.of<NavigationProvider>(context, listen: false)
+                .navigateToBackNavigationDestination(context);
+          },
+        ),
+        Text(
+          tr("mealsOverviewDaily_appBarBackNavigationText"),
+          style: TextStyle(
+            color: kColors_backNavigationTextColor,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// Builds the title widget for the `AppBar`, which includes a logo and the screen title.
+  Center _buildAppBarTitleWidget() {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Material(
+            type: MaterialType.transparency,
+            child: Hero(
+              tag: 'logo',
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('assets/images/meal_tracker_demo_icon_circle_400px.png'),
+                backgroundColor: kColors_backgroundColorGrey,
+              ),
+            ),
+          ),
+          const SizedBox(width: 10), // Spacing between the Hero image and title
+          Text(
+            tr("mealsOverviewDaily_appBarTitle"),
+            style: const TextStyle(
+              color: kColors_defaultTextColor,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// Builds the language switcher widget for the `AppBar`.
+  Widget _buildLanguageSwitcher(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16.0), // Optional padding for spacing
+      child: LanguageSwitcher(
+        locale: context.locale,
+        dropDownIconColor: Colors.blue[700]?.withOpacity(0.8),
+        flagWidth: 34,
+      ),
+    );
+  }
+
+  /// Builds the bottom widget for the `AppBar`, which displays a progress bar.
+  PreferredSize _buildAppBarBottomWidget() {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(4.0),
+      child: Container(
+        color: kColors_mealsOverViewScreenDaily_appBar_progressBar_color, // Progress bar color
+        height: 4.0,
+      ),
+    );
+  }
+
   /// Fetches the necessary data to populate the screen.
   ///
   /// This method retrieves the logged-in user data and updates the UI readiness flag
-  /// to indicate when the Meals Overview Daily Screen is ready.
-  /// The UI readiness state is managed by [UIReadinessProvider].
-  ///
-  /// It ensures that providers are properly initialized before accessing them.
+  /// to indicate when the `MealsDailyOverviewScreen` is ready.
   Future<void> _getData() async {
     // Ensure the widget is mounted before accessing context.
     if (!mounted) {
